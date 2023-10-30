0624aea5c7be034983b37ce1fc38ac2000b0c8cb
pimpaQ <yamaletidel@gmail.com>
2023-10-30 15:22:07 +0500

    фильтрация + сортировка

--- Глазки-Saves/ServicePage.xaml.cs
+++ Глазки-Saves/ServicePage.xaml.cs
@@ -23,11 +23,83 @@ namespace Глазки_Saves
         public ServicePage()
         {
             InitializeComponent();
+            var current_Agent = EyesEntities.GetContext().Agent.ToList();
+            AgentListView.ItemsSource = current_Agent;
+            CB_Filtr.SelectedIndex = 0;
+            CB_Sort.SelectedIndex = 0;
+            Update_Agent();
+        }
+        public void Update_Agent()
+        {
+            var current_Agent = EyesEntities.GetContext().Agent.ToList();
+
+            if(CB_Sort.SelectedIndex == 1)
+            {
+                current_Agent = current_Agent.OrderBy(p => p.Title).ToList();
+            }
+            if (CB_Sort.SelectedIndex == 2)
+            {
+                current_Agent = current_Agent.OrderByDescending(p => p.Title).ToList();
+            }
+            if (CB_Sort.SelectedIndex == 3)
+            {
+                current_Agent = current_Agent.OrderBy(p => p.Title).ToList();
+            }
+            if (CB_Sort.SelectedIndex == 4)
+            {
+                current_Agent = current_Agent.OrderBy(p => p.Title).ToList();
+            }
+            if (CB_Sort.SelectedIndex == 5)
+            {
+                current_Agent = current_Agent.OrderBy(p => p.Priority).ToList();
+            }
+            if (CB_Sort.SelectedIndex == 6)
+            {
+                current_Agent = current_Agent.OrderByDescending(p => p.Priority).ToList();
+            }
+            current_Agent = current_Agent.Where(p => p.Title.ToLower().Contains(TB_Search.Text.ToLower())).ToList();
+            if(CB_Filtr.SelectedIndex == 1)
+            {
+                current_Agent = current_Agent.Where(p => p.AgentTypeString == "МФО").ToList();
+            }
+            if (CB_Filtr.SelectedIndex == 2)
+            {
+                current_Agent = current_Agent.Where(p => p.AgentTypeString == "ООО").ToList();
+            }
+            if (CB_Filtr.SelectedIndex == 3)
+            {
+                current_Agent = current_Agent.Where(p => p.AgentTypeString == "ЗАО").ToList();
+            }
+            if (CB_Filtr.SelectedIndex == 4)
+            {
+                current_Agent = current_Agent.Where(p => p.AgentTypeString == "МКК").ToList();
+            }
+            if (CB_Filtr.SelectedIndex == 5)
+            {
+                current_Agent = current_Agent.Where(p => p.AgentTypeString == "ОАО").ToList();
+            }
+            if (CB_Filtr.SelectedIndex == 6)
+            {
+                current_Agent = current_Agent.Where(p => p.AgentTypeString == "ПАО").ToList();
+            }
+
+            AgentListView.ItemsSource = current_Agent;
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void TB_Search_TextChanged(object sender, TextChangedEventArgs e)
         {
-            Manager.MainFrame.Navigate(new AddEditPage());
+            Update_Agent();
+
+        }
+
+        private void CB_Sort_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Update_Agent();
+        }
+
+        private void CB_Filtr_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Update_Agent();
         }
     }
 }
