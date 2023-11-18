69acbf591ebcafd80f34ca234b35f678c59927aa
pimpaQ <yamaletidel@gmail.com>
2023-11-18 14:39:46 +0500

    апап

--- Глазки-Saves/ProductSale.cs
+++ Глазки-Saves/ProductSale.cs
@@ -11,7 +11,8 @@ namespace Глазки_Saves
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.Linq;
+
     public partial class ProductSale
     {
         public int ID { get; set; }
@@ -22,5 +23,77 @@ namespace Глазки_Saves
     
         public virtual Agent Agent { get; set; }
         public virtual Product Product { get; set; }
+        private static EyesEntities _context;
+
+        public static EyesEntities GetContext()
+        {
+            if (_context == null)
+            {
+                _context = new EyesEntities();
+            }
+            return _context;
+        }
+        public int SumProd
+        {
+            get
+            {
+                var agentIDsWithSales = EyesEntities.GetContext().ProductSale.Select(s => s.AgentID).Distinct();
+                var agentIDsWithoutSales = EyesEntities.GetContext().Agent
+                    .Where(agent => !agentIDsWithSales.Contains(agent.ID))
+                    .Select(agent => agent.ID);
+
+                int agentID = this.AgentID;
+
+                int sum = 0;
+
+                // Проверяем, есть ли продажи для текущего AgentID
+                if (agentIDsWithoutSales.Contains(agentID))
+                {
+                    return 0;
+                }
+                else
+                {
+                    sum = EyesEntities.GetContext().ProductSale
+                        .Where(s => s.AgentID == agentID)
+                        .Sum(s => s.ProductCount);
+                    return sum;
+                }
+            }
+        }
+        public int Discount
+        {
+            get
+            {
+                var sumQuery = EyesEntities.GetContext().ProductSale
+                .Join(EyesEntities.GetContext().Product,
+                sale => sale.ProductID,
+                product => product.ID,
+                (sale, product) => new { sale, product })
+                .Where(joined => joined.sale.AgentID == this.AgentID)
+                .GroupBy(joined => joined.sale.AgentID)
+                .Select(grouped => new
+                {
+                    AgentID = grouped.Key,
+                    Total = grouped.Sum(x => x.sale.ProductCount * x.product.MinCostForAgent)
+                });
+                int sum = (int)(sumQuery.FirstOrDefault()?.Total ?? 0);
+
+                int Disc = 0;
+
+                if (sum > 10000 && sum < 50000)
+                    Disc = 5;
+                else if (sum > 50000 && sum < 150000)
+                    Disc = 10;
+                else if (sum > 150000 && sum < 500000)
+                    Disc = 20;
+                else if (sum > 500000)
+                    Disc = 25;
+                else
+                    Disc = 0;
+                return Disc;
+            }
+        }
+
     }
+
 }
